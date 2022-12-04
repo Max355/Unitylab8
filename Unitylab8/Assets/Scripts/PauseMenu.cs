e2f5e722e7478ef1297dff4e71af0326755d188b
Max Yevtushok <73525840+Max355@users.noreply.github.com>
2022-12-04 06:00:51 +0200

    PauseMenu

---
+++ Unitylab8/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseCanvas;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+            Cursor.lockState = CursorLockMode.Confined;
+            
+        }
+    }
+
+    public void Stop()
+    {
+        PauseCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        Paused = true;
+        
+        
+    }
+
+    public void Play()
+    {
+       PauseCanvas.SetActive(false);
+       Time.timeScale = 1f;
+       Paused = false;
+       
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        
+    }
+}
